783a3ba7f2e0e7a2f7212b9a4c38f9aa366ab1fd
Raagnaa <egarcia4@byupathway.edu>
2026-03-31 20:56:20 -0500

    Remove review page and associated script for dynamic content

--- project/scripts/review.js
+++
@@ -1,7 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-
-document.getElementById('destination').textContent = params.get('product');
-document.getElementById('traveler').textContent = params.get('traveler');
-document.getElementById('visitdate').textContent = params.get('visitdate');
-document.getElementById('activities').textContent = params.getAll('activities').join(', ');
-document.getElementById('comments').textContent = params.get('comments');
